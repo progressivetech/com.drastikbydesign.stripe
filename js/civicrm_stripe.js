5d411ff79f0cceaeaedba9ccb8feee8b947ba7a1
Jamie McClelland <jm@mayfirst.org>
2017-06-20 15:01:49 -0400

    handle situations with no price (e.g. discount)
    https://github.com/drastik/com.drastikbydesign.stripe/issues/215

--- js/civicrm_stripe.js
+++ js/civicrm_stripe.js
@@ -152,13 +152,14 @@
       if ($('#priceset').length) {
         additionalParticipants = cj("#additional_participants").val();
         // The currentTotal is already being calculated in Form/Contribution/Main.tpl.
-        if(typeof currentTotal !== 'undefined') {
-          if (currentTotal == 0 && !additionalParticipants) {
-            // This is also hit when "Going back", but we already have stripe_token.
-            debugging('ozlkf');
-            // This should not happen on Confirm Contribution, but seems to on 4.6 for some reason.
-            //return true;
-          }
+        if(typeof currentTotal === 'undefined' && $('#pricevalue').length) {
+          currentTotal = $('#pricevalue').data('raw-total');
+        }
+        if (currentTotal == 0 && !additionalParticipants) {
+          // This is also hit when "Going back", but we already have stripe_token.
+          debugging('ozlkf');
+          // This should not happen on Confirm Contribution, but seems to on 4.6 for some reason.
+          return true;
         }
       }
 
